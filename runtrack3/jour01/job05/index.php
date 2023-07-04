61c8d34a0646914fd9634331e207c8b964a4d991
adil-bourichi <adil.bourichi@laplateforme.io>
2023-07-04 11:40:21 +0200

    job5

---
+++ runtrack3/jour01/job05/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Affichage des jours de la semaine</title>
+  <script src="script.js"></script>
+</head>
+<body>
+  <h1>Affichage des jours de la semaine</h1>
+
+  <script>
+    afficherjourssemaines();
+  </script>
+</body>
+</html>
