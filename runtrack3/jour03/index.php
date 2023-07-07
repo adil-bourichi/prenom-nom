c275ea2c741cb5f2c49c157aa512a971b6610366
adil-bourichi <adil.bourichi@laplateforme.io>
2023-07-07 10:59:43 +0200

    job1

---
+++ runtrack3/jour03/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
+    <title>Document</title>
+    <script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+    <button id="showButton">Afficher le texte</button>
+    <div id="textContainer" style="display: none;">
+      Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit, ensuite on prie.
+    </div>
+    <button id="hideButton">Cacher le texte</button>
+</body>
+</html>
