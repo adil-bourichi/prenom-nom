d90a93d98fc529ccb7dfc1ae7e385f384a9587db
adil-bourichi <adil.bourichi@laplateforme.io>
2023-07-29 21:17:00 +0200

    job2

---
+++ runtrack3/jour08/job02/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulaire de création de compte</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    <header class="bg-blue-500 p-4 flex justify-between items-center">
+        <nav>
+            <ul class="flex space-x-4">
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="index.php">Inscription</a></li>
+                <li><a href="index.php">Connexion</a></li>
+                <li><a href="index.php">Rechercher</a></li>
+            </ul>
+        </nav>
+        <div class="text-white">Mon Logo</div>
+    </header>
+
+    <section>
+    </section>
+
+    <footer>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="index.php">Inscription</a></li>
+            <li><a href="index.php">Connexion</a></li>
+            <li><a href="index.php">Rechercher</a></li>
+        </ul>
+    </footer>
+
+</body>
+</html>
